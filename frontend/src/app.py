07b5e3c679e148805322b500b2bf8b9dc6d18415
XavierJCallait <xaviercallait@gmail.com>
2026-02-07 15:09:54 -0500

    updated rtsp and urls in app.py to fix misspellings and updated all_cameras.html to allow for cv camera viewing

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -43,20 +43,20 @@ class Frontend(Node):
 
         # Get camera URLs from environment variables or use defaults
         self.camera_urls = {
+            'cv_camera': os.getenv('CV_CAMERA_URL', 'http://localhost:8889/cv_camera'),
             'camera1': os.getenv('CAMERA1_URL', 'http://localhost:8889/camera_1'),
             'camera2': os.getenv('CAMERA2_URL', 'http://localhost:8889/camera_2'),
             'camera3': os.getenv('CAMERA3_URL', 'http://localhost:8889/camera_3'),
-            'camera4': os.getenv('CAMERA4_URL', 'http://localhost:8889/camera_4'),
-            'cv_camera': os.getenv('CV_CAMEERA_URL', 'http://localhost:8889/cv_camera')
+            'camera4': os.getenv('CAMERA4_URL', 'http://localhost:8889/camera_4')
         }
 
         # For FFmpeg snapshot
         self.camera_rtsp_urls = {
+            "cv_camera": "rtsp://127.0.0.1:8554/cv_camera",
             "camera1": "rtsp://127.0.0.1:8554/camera_1",
             "camera2": "rtsp://127.0.0.1:8554/camera_2",
             "camera3": "rtsp://127.0.0.1:8554/camera_3",
-            "camera4": "rtsp://127.0.0.1:8554/camera_4",
-            "cv_camera": "rstp://127.0.0.1:8554/cv_camera"
+            "camera4": "rtsp://127.0.0.1:8554/camera_4"
         }        
 
         self.get_logger().info(f"Camera URLs: {self.camera_urls}")
